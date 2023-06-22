950c042c2a589fc4fe7dddd905e4ab1df4436169
Roxana <roxana.gherman@fidasolutions.com>
2023-06-22 11:05:07 +0300

    use simplified return and chained fetch/then

--- index.js
+++ index.js
@@ -37,26 +37,18 @@ function clickOnMenu(e) {
 function showSkillsList(skills) {
   const ul = $("#skills ul");
   const skillsHTML = skills.map((skill) => {
-    // console.info("inside map", skill);
-
-    // <li class="favorite">HTML</li>
     const cls = skill.favorite ? "favorite" : "";
     return `<li class="${cls}">${skill.name} <span> · ${skill.endorcements} </span> </li>`;
   });
 
-  //   console.warn(skillsHTML);
   skillsHTML.push("<li>...</li>");
   ul.innerHTML = skillsHTML.join("");
 }
 
 function getSkillsRequest() {
-  //fetch ne aduce date de pe server din fisierul skills.json
-  fetch("skills.json").then((r) => {
-    // console.info("done");
-    r.json().then(showSkillsList);
-  });
-  //   console.warn("am sau nu info?", skills);
-  //   console.warn("todo get skills");
+  fetch("skills.json")
+    .then((r) => r.json())
+    .then(showSkillsList);
 }
 
 displayPage(activePage);
